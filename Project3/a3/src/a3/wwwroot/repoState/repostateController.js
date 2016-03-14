828a8e17294c427a1ae217b863e05eb1e079788a
songssam <songssam@gmail.com>
2016-03-14 16:03:57 -0500

    FINAL

--- Project3/a3/src/a3/wwwroot/repoState/repostateController.js
+++ Project3/a3/src/a3/wwwroot/repoState/repostateController.js
@@ -7,42 +7,51 @@
             $scope.login = login;
             $scope.repo = repo;
             $scope.issueName ={};
-
+            $scope.showTable;
 
             $scope.data = {
-                saveData: []
-            }
-            $scope.issues = {
+                saveData: [],
                 saveIssues: []
             }
+            
 
             $scope.data.saveData = repo;
-
-            console.log('Y U P ' + userService);
 
          
             $scope.maxSize = 10;
             $scope.bigTotalItems = $scope.login.open_issues_count;
             $scope.bigCurrentPage = 1;
             $scope.numPages;
-
-          
-            getIssue().then(
-                 function (issueName){
-                     $scope.issues.saveIssues = issueName;
-                     console.log('BAAAM' + issueName);
-                     if ($scope.check == 0) {
-                     }
-                 });
-            
+            if ($scope.login.open_issues_count == 0) {
+                $scope.showTable = 0;
+            }
+            else {
+                $scope.showTable = 1;
+            }
+                getIssue().then(
+                     function (issueName) {
+                         $scope.data.saveIssues = issueName;
+                         console.log('BAAAM' + issueName);
+                         if ($scope.check == 0) {
+                         }
+                     });
+            $scope.updateIssue = function () {
+                getIssue().then(
+                     function (issueName) {
+                         $scope.data.saveIssues = issueName;
+                         console.log('BAAAM' + issueName);
+                         if ($scope.check == 0) {
+                         }
+                     });
+            }
             function getIssue() {
                 var deferred = $q.defer();
                 $http.get('https://api.github.com/repos/' + $scope.login.full_name + '/issues?page=' + $scope.bigCurrentPage + '&per_page=10').then(
                     function (response) {
                         console.log('github response user STATE info 1111 ' + $scope.login.full_name);
-                        console.log('>>>>>>> https://api.github.com/repos/' + $scope.login.full_name + '/issues?page=' + $scope.bigCurrentPage + '&per_page=10');
-                        console.log('There 111' + response.issues);
-                        deferred.resolve(response.issues);
+                        console.log('>>>>> https://api.github.com/repos/' + $scope.login.full_name + '/issues?page=' + $scope.bigCurrentPage + '&per_page=10');
+                        console.log('There 111' + response.data);
+                        deferred.resolve(response.data);
                     });
                 return deferred.promise;
             }
